a00e3f297a33746797e8686dc2eb364a4ceb4362
italianman <nino.avitabile1997@gmail.com>
2019-04-09 13:14:06 +0200

    Changed some css files and added loyaltyprovider component

--- src/app/loyalty-providers/loyalty-providers.component.ts
+++ src/app/loyalty-providers/loyalty-providers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { User, LoyaltyProvider } from '../models/loyaltynetwork';
 
 @Component({
   selector: 'app-loyalty-providers',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loyalty-providers.component.css']
 })
 export class LoyaltyProvidersComponent implements OnInit {
+  private errorMessage;
+  allProviders: Array<LoyaltyProvider> = [];
 
-  constructor() { }
+  constructor(private httpService: HttpService) { 
+    this.getAllProviders();
+  }
 
   ngOnInit() {
   }
+
+  async getAllProviders(): Promise<any> {
+    const tempList = [];
+    try {
+      const result = await this.httpService.getAll('LoyaltyProvider')
+        .toPromise();
+      this.errorMessage = null;
+      result.forEach(user => {
+        tempList.push(user);
+      });
+      this.allProviders = tempList;
+    }
+    catch (error) {
+      if (error === 'Server error') {
+        this.errorMessage = 'Could not connect to REST server. Please check your configuration details';
+      }
+      else if (error === '404 - Not Found') {
+        this.errorMessage = '404 - Could not find API route. Please check your available APIs.';
+        this.errorMessage = error;
+      }
+    }
+  }
+
 
 }
