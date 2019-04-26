599f07137ff68e74708162c11d02f398424a8255
Nino <nino.avitabile1997@gmail.com>
2019-04-26 18:17:08 +0200

    Changed the retrieving of the providers as well as the logic to delete & join

--- src/app/customer/loyalty-program-overview/loyalty-program-overview.component.ts
+++ src/app/customer/loyalty-program-overview/loyalty-program-overview.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Type } from '@angular/compiler';
 import { Observable } from 'rxjs';
 import { LoyaltyProvider, Customer } from 'src/app/models/loyaltynetwork';
 import { LoyaltyproviderService } from 'src/app/services/loyaltyprovider.service';
 import { CustomerService } from 'src/app/services/customer.service';
+import { Router } from '@angular/router';
+import { TransactionService } from 'src/app/services/transaction.service';
 
 @Component({
   selector: 'app-loyalty-program-overview',
@@ -10,45 +13,69 @@ import { CustomerService } from 'src/app/services/customer.service';
   styleUrls: ['./loyalty-program-overview.component.css']
 })
 export class LoyaltyProgramOverviewComponent implements OnInit {
-  private allProviders: LoyaltyProvider[];
-  private signedInCustomer: Customer;
-  private providersToJoin: LoyaltyProvider[];
+  public allProviders: Observable<LoyaltyProvider[]>;
+  private signedInCustomer;
+  public providersToJoin: LoyaltyProvider[] = [];
+  private joinTransaction;
+  private exitTransaction;
 
-  constructor(private loyaltyProviderService: LoyaltyproviderService, private customerProviderService: CustomerService) {
+  constructor(private loyaltyProviderService: LoyaltyproviderService, private customerService: CustomerService, private router: Router,
+              private transactionService: TransactionService) { }
+
+  ngOnInit() {
     this.getCustomer();
-    //this.getProvidersToJoin();
+    this.getProviders();
   }
 
-  ngOnInit() { }
-
   getProviders() {
-    this.loyaltyProviderService.getAllProviders().subscribe(providers => {
-      this.allProviders = providers;
-      console.log(this.allProviders);
-    });
+    this.allProviders = this.loyaltyProviderService.getAllProviders();
   }
 
   getCustomer() {
-    this.customerProviderService.getCustomer('Sentjens50')
-    .toPromise()
-    .then(customer => {
-      this.getProviders();
+    this.customerService.getCustomer('Henk1').subscribe(customer => {
+      this.signedInCustomer = customer;
     });
   }
 
-  getProvidersToJoin() {
-    this.signedInCustomer.providers.forEach(provider1 => {
-      this.allProviders.forEach(provider2 => {
-        console.log(provider2);
-        if (provider1.userId !== provider2.userId) {
-          this.providersToJoin.push(provider1);
-       }
+  checkIfJoined(loyaltyProvider: LoyaltyProvider): boolean {
+    let hasJoined = false;
+    this.signedInCustomer.providers.forEach(provider => {
+        const userId: string = provider.split('#')[1];
+        const userId2: string = loyaltyProvider.userId;
+        if (userId === userId2) {
+          hasJoined = true;
+        }
       });
-    });
 
+    return hasJoined;
   }
 
+  joinProvider(loyaltyProvider: LoyaltyProvider) {
+    this.joinTransaction = {
+      $class: 'loyaltynetwork.joinProgram',
+      programOwner: 'resource:loyaltynetwork.LoyaltyProvider#' + loyaltyProvider.userId,
+      joiner: 'resource:loyaltynetwork.Customer#' + this.signedInCustomer.userId,
+    };
 
+    this.transactionService.joinProgram(this.joinTransaction)
+    .toPromise()
+    .then(() => {
+        this.router.navigateByUrl('home');
+      });
+  }
 
+  deleteProvider(loyaltyProvider: LoyaltyProvider) {
+    this.exitTransaction = {
+      $class: 'loyaltynetwork.exitProgram',
+      programOwner: 'resource:loyaltynetwork.LoyaltyProvider#' + loyaltyProvider.userId,
+      exiter: 'resource:loyaltynetwork.Customer#' + this.signedInCustomer.userId,
+    };
+
+    this.transactionService.exitProgram(this.exitTransaction)
+      .toPromise()
+      .then(() => {
+        this.router.navigateByUrl('home');
+      });
+  }
 
 }
