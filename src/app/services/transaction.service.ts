de686cadf0d331ea2d6d5c1d81dc205d5d7f67a5
Nino <nino.avitabile1997@gmail.com>
2019-05-22 11:34:26 +0200

    Transaction service
    Made it possible to retrieve all transaction of a certain transaction type at once

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,35 +1,62 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
-import { joinProgram } from '../models/loyaltynetwork';
+import { joinProgram, earnTokens } from '../models/loyaltynetwork';
 import { HttpService } from './http.service';
-import { Type } from '@angular/compiler';
+import { Transaction } from '../models/hyperledger.composer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
-  constructor(private httpService: HttpService<Type>) { }
+  constructor(private httpService: HttpService<Transaction>) { }
   
-  joinProgram(instance: Type): Observable<Type> {
+  joinProgram(instance: Transaction): Observable<Transaction> {
     return this.httpService.addSingleInstance('joinProgram', instance);
   }
 
-  exitProgram(instance: Type): Observable<Type> {
+  registerForAProgram(instance: Transaction): Observable<Transaction> {
+    return this.httpService.addSingleInstance('registerForAProgram', instance);
+  }
+
+  declineForAProgram(instance: Transaction): Observable<Transaction> {
+    return this.httpService.addSingleInstance('declineForAProgram', instance);
+  }
+
+  exitProgram(instance: Transaction): Observable<Transaction> {
     return this.httpService.addSingleInstance('exitProgram', instance);
   }
 
-  earnTokens(instance: Type): Observable<Type> {
+  earnTokens(instance: Transaction): Observable<Transaction> {
     return this.httpService.addSingleInstance('earnTokens', instance);
   }
 
-  redeemTokens(instance: Type): Observable<Type> {
+  getAllEarnTokens(filter: string): Observable<any[]> {
+    return this.httpService.getAll('earnTokens', filter);
+  }
+
+  getAllRedeemTokens(filter: string): Observable<any[]> {
+    return this.httpService.getAll('redeemTokens', filter);
+  }
+
+  getAllTradeTokens(filter: string): Observable<any[]> {
+    return this.httpService.getAll('tradeTokens', filter);
+  }
+
+  getAllIssueTokens(filter: string): Observable<any[]> {
+    return this.httpService.getAll('issueTokens', filter);
+  }
+
+  redeemTokens(instance: Transaction): Observable<Transaction> {
     return this.httpService.addSingleInstance('redeemTokens', instance);
   }
 
-
-  issueTokens(instance: Type): Observable<Type> {
+  issueTokens(instance: Transaction): Observable<Transaction> {
     return this.httpService.addSingleInstance('issueTokens', instance);
+  }
+
+  returnTransactionsByUser(instance: Transaction, options?: Transaction): Observable<Transaction> {
+    return this.httpService.addSingleInstance('returnTransactionsByUser', instance, options);
   }
 }
