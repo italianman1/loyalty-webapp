7ce70f8b015002f14934bb7d2f84a1d4e80896ee
Nino <nino.avitabile1997@gmail.com>
2019-05-22 12:15:24 +0200

    Removed unnecessary dummy data and code

--- src/app/customer/token-overview/token-overview.component.ts
+++ src/app/customer/token-overview/token-overview.component.ts
@@ -4,6 +4,7 @@ import { QueryService } from 'src/app/services/query.service';
 import { CustomerService } from 'src/app/services/customer.service';
 import { LoyaltyproviderService } from 'src/app/services/loyaltyprovider.service';
 import { TokenService } from 'src/app/services/token.service';
+import { SessionService } from 'src/app/services/session.service';
 
 @Component({
   selector: 'app-token-overview',
@@ -15,40 +16,27 @@ export class TokenOverviewComponent implements OnInit {
   private pointsSpentData: PointsData[] = [];
   private signedInCustomer;
 
-  constructor(private queryService: QueryService, private customerService: CustomerService,
-              private loyaltyProviderService: LoyaltyproviderService, private tokenService: TokenService) { }
+  constructor(private queryService: QueryService, private sessionService: SessionService) { }
 
   ngOnInit() {
-    this.getCustomer();
-    const data: PointsData = {provider: 'Praxis', points: 100, euros: 200};
-    const data2: PointsData = {provider: 'Gamma', points: 100, euros: 150};
-    const data3: PointsData = {provider: 'Maxeda', points: 50, euros: 50};
-    const data4: PointsData = {provider: 'whazaaa', points: 80, euros: 160};
-    this.pointsEarnedData.push(data);
-    this.pointsSpentData.push(data2);
-    this.pointsSpentData.push(data3);
-    this.pointsSpentData.push(data4);
+    this.getUser();
+ 
   }
 
-  getCustomer() {
-    this.customerService.getCustomer('Henk1')
-    .toPromise()
-    .then(customer => {
-      this.signedInCustomer = customer;
-      //this.getPointsSpentData();
+  getUser() {
+    this.sessionService.getSignedInUser()
+    .then(user => {
+      this.signedInCustomer = user;
       this.getPointsEarnedData();
+      console.log(this.pointsEarnedData, this.signedInCustomer);
     });
   }
 
   getPointsSpentData() {
-    this.signedInCustomer.providers.forEach(providerResourceId => {
+    this.signedInCustomer.providers.forEach(provider => {
       let data: PointsData = {provider: '', points: 0, euros: 0};
-      const providerId: string = providerResourceId;
-      this.loyaltyProviderService.getProvider(providerId.split('#')[1])
-      .toPromise()
-      .then(provider => {
-        data.provider = provider.companyName;
-        this.queryService.selectAllRedeemedTokenTransactions(this.signedInCustomer.userId, provider.userId)
+      data.provider = provider.companyName;
+      this.queryService.selectAllRedeemedTokenTransactions(this.signedInCustomer.userId, provider.userId)
         .toPromise()
         .then(redeemTokens => {
           redeemTokens.forEach(transaction => {
@@ -56,53 +44,39 @@ export class TokenOverviewComponent implements OnInit {
           });
           data.euros = data.points / provider.conversionRate;
         });
-      });
       this.pointsSpentData.push(data);
-    });
-
-    console.log(this.pointsSpentData);
+      });
   }
 
   getPointsEarnedData() {
-    this.signedInCustomer.tokens.forEach(tokenId => {
-      this.tokenService.getToken(tokenId.split('#')[1])
-      .toPromise()
-      .then(token => {
-        if (this.pointsEarnedData.length == 0) {
-          let data: PointsData = {provider: '', points: 0, euros: 0};
-          this.loyaltyProviderService.getProvider(token.issuer.split('#')[1])
-          .toPromise()
-          .then(provider => {
-            data.provider = provider.companyName;
-            data.points = 1;
-            data.euros = data.points / provider.conversionRate;
+    this.signedInCustomer.tokens.forEach(token => {
+      if (this.pointsEarnedData.length == 0) {
+        let data: PointsData = {provider: '', points: 0, euros: 0};
+        data.provider = token.issuer.companyName;
+        data.points = 0;
+        data.euros = data.points / token.issuer.conversionRate;
+        this.pointsEarnedData.push(data);
+        console.log(data);
+      }
+
+      if (this.pointsEarnedData.length > 0) {
+          let found = false;
+          this.pointsEarnedData.forEach(data => {
+            if(data.provider == token.issuer.companyName) {
+              data.points += 1;
+              data.euros = data.points / token.issuer.conversionRate;
+              found = true;
+              return;
+            }});
+
+          if (!found) {
+            let data: PointsData = {provider: token.issuer.companyName, points: 1, euros: 0};
+            data.euros = data.points / token.issuer.conversionRate;
+            console.log(data.points);
+            console.log(token.issuer.conversionRate);
             this.pointsEarnedData.push(data);
-          });
-        }
-
-        if (this.pointsEarnedData.length > 0) {
-          this.loyaltyProviderService.getProvider(token.issuer.split('#')[1])
-          .toPromise()
-          .then(provider => {
-            console.log(provider);
-            let found = false;
-            this.pointsEarnedData.forEach(data => {
-              if(data.provider == provider.companyName) {
-                data.points += 1;
-                data.euros = data.points / provider.conversionRate;
-                found = true;
-              }});
-
-            if (!found) {
-              let data: PointsData = {provider: provider.companyName, points: 1, euros: 0};
-              data.euros = data.points / provider.conversionRate;
-              console.log(data.points);
-              console.log(provider.conversionRate);
-              this.pointsEarnedData.push(data);
-            }
-          });
-         }
-      });
+          }
+      }
     });
   }
 }
