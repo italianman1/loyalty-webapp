b755ae1ebc246f47f0b35501db7c3e02ede00092
Nino <nino.avitabile1997@gmail.com>
2019-05-22 12:08:11 +0200

    Perform transaction
    Made it possible to earn and redeem tokens at partners and to add a (four-way possible) discount

--- src/app/customer/perform-transaction/perform-transaction.component.ts
+++ src/app/customer/perform-transaction/perform-transaction.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { LoyaltyproviderService } from 'src/app/services/loyaltyprovider.service';
-import { LoyaltyProvider, issueTokens } from 'src/app/models/loyaltynetwork';
+import { LoyaltyProvider, User } from 'src/app/models/loyaltynetwork';
 import { CustomerService } from 'src/app/services/customer.service';
 import { TransactionService } from 'src/app/services/transaction.service';
 import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
+import { PartnerService } from 'src/app/services/partner.service';
 
 @Component({
   selector: 'app-perform-transaction',
@@ -11,101 +13,218 @@ import { Router } from '@angular/router';
   styleUrls: ['./perform-transaction.component.css']
 })
 export class PerformTransactionComponent implements OnInit {
-  providers: LoyaltyProvider[];
+  private retailers: any[] = [];
   private redeemTransaction;
   private earnTransaction;
   private issueTransaction;
   private amountOfGroceries;
-  private selectedProviderId;
+  private selectedRetailerId;
   private signedInCustomer;
-  private tokensToBeRedeemed;
+  private offerTransaction;
+  private amountOfDiscount;
+  private errorMessage = '';
+  private clicked = false;
 
-  constructor(private loyaltyProviderService: LoyaltyproviderService, private customerService: CustomerService,
+  constructor(private loyaltyProviderService: LoyaltyproviderService, private sessionService: SessionService,
               private transactionService: TransactionService, private router: Router) {
   }
 
   ngOnInit() {
-   this.getProviders();
-   this.getCustomer();
+   this.getProvidersAndPartners();
+   this.getUser();
+   console.log(this.retailers);
   }
 
-  getProviders() {
-    this.loyaltyProviderService.getAllProviders().subscribe(providers => {
-      this.providers = providers;
+  getProvidersAndPartners() {
+    this.loyaltyProviderService.getAllProviders('{"include": "resolve"}').subscribe(providers => {
+      providers.forEach(provider => {
+        this.retailers.push(provider);
+        provider.partners.forEach(partner => {
+          this.retailers.push(partner);
+        });
+      });
     });
   }
 
-  getCustomer() {
-    this.customerService.getCustomer('Henk1').subscribe(customer => {
-      this.signedInCustomer = customer;
+  getUser() {
+    this.sessionService.getSignedInUser()
+    .then(user => {
+      this.signedInCustomer = user;
     });
+  }
+
+  goBack() {
+    this.router.navigateByUrl('home');
   }
 
   onSubmit() {
-    let selectedProvider;
-    this.providers.forEach(provider => {
-      if (provider.userId == this.selectedProviderId) {
-        selectedProvider = provider;
-      }});
+    this.clicked = true;
+    let selectedRetailer;
 
-    if (this.amountOfGroceries > 0 && this.tokensToBeRedeemed > 0) {
-      this.issueTokens(selectedProvider)
+    this.retailers.forEach(retailer => {
+      if (retailer.userId == this.selectedRetailerId) {
+        selectedRetailer = retailer;
+      }
+    });
+
+    if ((this.amountOfGroceries - this.amountOfDiscount) > 0 && this.amountOfDiscount > 0) {
+      this.issueTokens(selectedRetailer)
       .then(() => {
-        this.redeemTokens(selectedProvider)
+        this.redeemTokens(selectedRetailer)
         .then(() => {
-          this.earnTokens(selectedProvider);
-          this.router.navigateByUrl('/home');
+          this.earnTokens(selectedRetailer);
+          this.router.navigateByUrl('/transaction-overview');
+        }).catch(err => {
+          this.showError(err);
         });
+      }).catch(err => {
+        this.showError(err);
       });
     }
 
-    if (this.amountOfGroceries > 0 && this.tokensToBeRedeemed == 0) {
-        this.issueTokens(selectedProvider)
+    if ((this.amountOfGroceries - this.amountOfDiscount) > 0 && this.amountOfDiscount == 0) {
+        this.issueTokens(selectedRetailer)
         .then(() => {
-          this.earnTokens(selectedProvider);
-          this.router.navigateByUrl('/home');
+          this.earnTokens(selectedRetailer);
+          this.router.navigateByUrl('/transaction-overview');
+        }).catch(err => {
+          this.showError(err);
         });
       }
 
-    if (this.amountOfGroceries == 0 && this.tokensToBeRedeemed > 0) {
-        this.redeemTokens(selectedProvider)
+    if ((this.amountOfGroceries - this.amountOfDiscount) <= 0 && this.amountOfDiscount > 0) {
+        this.redeemTokens(selectedRetailer)
         .then(() => {
-          this.router.navigateByUrl('/home');
+          this.router.navigateByUrl('/transaction-overview');
+        }).catch(err => {
+          this.showError(err);
         });
       }
   }
 
-  issueTokens(provider: LoyaltyProvider): Promise<any> {
-    this.issueTransaction = {
-      $class: 'loyaltynetwork.issueTokens',
-      issuedTokens: this.amountOfGroceries / provider.conversionRate,
-      issuer: 'resource:loyaltynetwork.LoyaltyProvider#' + provider.userId,
-    };
+  issueTokens(retailer: any): Promise<any> {
+    if(retailer.role == 'Partner') {
+      this.issueTransaction = {
+        $class: 'loyaltynetwork.issueTokens',
+        issuedTokens: (this.amountOfGroceries - this.amountOfDiscount) * retailer.provider.conversionRate,
+        issuer: 'resource:loyaltynetwork.LoyaltyProvider#' + retailer.provider.userId,
+        amountOfEuros: (this.amountOfGroceries - this.amountOfDiscount) / retailer.provider.conversionRate,
+      };
+    }
+
+    else { 
+      this.issueTransaction = {
+        $class: 'loyaltynetwork.issueTokens',
+        issuedTokens: (this.amountOfGroceries - this.amountOfDiscount) * retailer.conversionRate,
+        issuer: 'resource:loyaltynetwork.LoyaltyProvider#' + retailer.userId,
+        amountOfEuros: (this.amountOfGroceries - this.amountOfDiscount) / retailer.conversionRate,
+      };
+    }
+
+    console.log(this.issueTokens);
 
     return this.transactionService.issueTokens(this.issueTransaction).toPromise();
   }
 
-  redeemTokens(provider: LoyaltyProvider): Promise<any> {
-    console.log(this.tokensToBeRedeemed);
-    this.redeemTransaction = {
-      $class: 'loyaltynetwork.redeemTokens',
-      redeemedTokens: this.tokensToBeRedeemed,
-      accepter: 'resource:loyaltynetwork.LoyaltyProvider#' + provider.userId,
-      redeemer: 'resource:loyaltynetwork.Customer#' + this.signedInCustomer.userId,
-    };
+  redeemTokens(retailer: any): Promise<any> {
+    if(retailer.role == "Partner") {
+      this.redeemTransaction = {
+        $class: 'loyaltynetwork.redeemTokens',
+        accepter: 'resource:loyaltynetwork.LoyaltyPartner#' + encodeURI(retailer.userId),
+        redeemer: 'resource:loyaltynetwork.Customer#' + encodeURI(this.signedInCustomer.userId),
+        amountOfDiscount: parseInt(this.amountOfDiscount, 10)
+      };
+    }
+
+    if(retailer.role == "Provider"){
+      this.redeemTransaction = {
+        $class: 'loyaltynetwork.redeemTokens',
+        accepter: 'resource:loyaltynetwork.LoyaltyProvider#' + encodeURI(retailer.userId),
+        redeemer: 'resource:loyaltynetwork.Customer#' + encodeURI(this.signedInCustomer.userId),
+        amountOfDiscount: parseInt(this.amountOfDiscount, 10)
+      };
+    }
+
+    console.log(this.redeemTransaction);
 
     return this.transactionService.redeemTokens(this.redeemTransaction).toPromise();
   }
 
-  earnTokens(provider: LoyaltyProvider): Promise<any> {
-    this.earnTransaction = {
-      $class: 'loyaltynetwork.earnTokens',
-      earner: 'resource:loyaltynetwork.Customer#' + this.signedInCustomer.userId,
-      earnedTokens: this.amountOfGroceries / provider.conversionRate,
-      issuer: 'resource:loyaltynetwork.LoyaltyProvider#' + provider.userId,
-    };
+  earnTokens(retailer: any): Promise<any> {
+    if(retailer.role == "Partner") {
+      this.earnTransaction = {
+        $class: 'loyaltynetwork.earnTokens',
+        earner: 'resource:loyaltynetwork.Customer#' + encodeURI(this.signedInCustomer.userId),
+        issuer: 'resource:loyaltynetwork.LoyaltyPartner#' + encodeURI(retailer.userId),
+        amountOfEuros: (this.amountOfGroceries - this.amountOfDiscount)
+      };
+    }
+
+    if(retailer.role == "Provider") {
+      this.earnTransaction = {
+        $class: 'loyaltynetwork.earnTokens',
+        earner: 'resource:loyaltynetwork.Customer#' + encodeURI(this.signedInCustomer.userId),
+        issuer: 'resource:loyaltynetwork.LoyaltyProvider#' + encodeURI(retailer.userId),
+        amountOfEuros: (this.amountOfGroceries - this.amountOfDiscount)
+      };
+    }
+    console.log(this.earnTransaction);
 
     return this.transactionService.earnTokens(this.earnTransaction).toPromise();
+  }
+
+  // performOffer1() {
+  //   let provider;
+  //   this.loyaltyProviderService.getProvider('Action0').subscribe(result => {
+  //     provider = result;
+  //   });
+
+  //   this.offerTransaction = {
+  //     $class: 'loyaltynetwork.redeemTokens',
+  //     redeemedTokens: 10,
+  //     accepter: 'resource:loyaltynetwork.LoyaltyProvider#' + encodeURI(provider.userId),
+  //     redeemer: 'resource:loyaltynetwork.Customer#' + encodeURI(this.signedInCustomer.userId),
+  //     amountOfEuros: 10
+  //   };
+  // }
+
+  // performOffer2() {
+  //     let provider;
+  //     this.loyaltyProviderService.getProvider('Praxis0').subscribe(result => {
+  //       provider = result;
+  //     });
+
+  //     this.offerTransaction = {
+  //       $class: 'loyaltynetwork.redeemTokens',
+  //       redeemedTokens: 50,
+  //       accepter: 'resource:loyaltynetwork.LoyaltyProvider#' + encodeURI(provider.userId),
+  //       redeemer: 'resource:loyaltynetwork.Customer#' + encodeURI(this.signedInCustomer.userId),
+  //       amountOfEuros: 100
+  //   }
+  // }
+
+  // performOffer3() {
+  //   let provider;
+  //   this.loyaltyProviderService.getProvider('Praxis0').subscribe(result => {
+  //     provider = result;
+  //   });
+
+  //   this.offerTransaction = {
+  //     $class: 'loyaltynetwork.earnTokens',
+  //     earner: 'resource:loyaltynetwork.Customer#' + encodeURI(this.signedInCustomer.userId),
+  //     earnedTokens: 100,
+  //     issuer: 'resource:loyaltynetwork.LoyaltyProvider#' + encodeURI(provider.userId),
+  //     amountOfEuros: 10
+  // }
+// }
+
+  setDiscount(discount) {
+    this.amountOfDiscount = discount;
+  }
+
+  showError(errorMessage) {
+    this.errorMessage = errorMessage;
+    this.router.navigateByUrl('/transaction-overview');
   }
 
 }
